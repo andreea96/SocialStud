be6e6647f405e328190b05d6f5804e35eacf36ca
andreea96 <andreea.olaru@zitec.com>
2017-10-24 20:58:31 +0300

    add all files

--- screens/CoursesListScreen.js
+++ screens/CoursesListScreen.js
@@ -1,32 +1,43 @@
-/**
- * Created by andreea.olaru on 10/24/2017.
- */
 import React from 'react';
-import { ScrollView, StyleSheet, SectionList } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet,SectionList,FlatList, Text,View } from 'react-native';
+import Card from "../node_modules/react-navigation/lib/views/CardStack/Card";
 
-export default class LinksScreen extends React.Component {
+
+export default class CoursesListScreen extends React.Component {
+
+
+
     static navigationOptions = {
         title: 'Courses',
     };
 
     render() {
+        let dataSource=[
+            {   Course1: 'AC', Sala: 'A01', Ora: '09:00' },
+            {   Course1: 'MAP', Sala: 'A02', Ora: '09:00' },
+            {   Course1: 'MS', Sala: 'A03', Ora: '09:00' },
+            {   Course1: 'PS', Sala: 'A04', Ora: '10:00' },
+        ];
         return (
             <ScrollView style={styles.container}>
-                <SectionList
-                    renderItem={({item}) => <ListItem title={item} />}
-                    renderSectionHeader={({section}) => <Header title={section.title} />}
-                    sections={[ // homogenous rendering between sections
-                        {data: 'data', title: 'title'},
-                    ]}
-                />
+                <FlatList style={styles.flatListContainer}
+                data={dataSource}
+                renderItem={
+                    ({item}) =>
 
-                <SectionList
-                    sections={[ // heterogeneous rendering between sections
-                        {data: 'data', renderItem: 'title'},
+                               <View style={styles.lineContainer}>
+                                <Text style={styles.textContainer}>
+                                    {item.Sala}
+                                </Text>
+                                   <Text>
+                                       {item.Course1}
+                                       </Text>
+                                   <Text>
+                                       {item.Ora}
+                                   </Text>
+                            </View>
 
-                    ]}
-                />
+            }/>
             </ScrollView>
         );
     }
@@ -37,5 +48,22 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 15,
         backgroundColor: '#fff',
+
     },
+    flatListContainer:{
+        flex: 1,
+    },
+    lineContainer: {
+        backgroundColor: '#ff69b4',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+        marginBottom: 10,
+        alignItems: 'center',
+
+    },
+    textContainer: {
+        fontSize: 20,
+
+    }
+
 });
